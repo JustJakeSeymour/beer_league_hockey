c33fa9dd0d8184d73c14ce461d983b35d06bbf90
J Seymour <JustJakeSeymour@gmail.com>
2022-12-04 12:09:50 -0700

    add user stories to spec files

---
+++ spec/features/divisions/show_spec.rb
@@ -0,0 +1,18 @@
+# User Story 2, Parent Show 
+
+# As a visitor
+# When I visit '/parents/:id'
+# Then I see the parent with that id including the parent's attributes
+# (data from each column that is on the parent table)
+
+# User Story 7, Parent Child Count
+
+# As a visitor
+# When I visit a parent's show page
+# I see a count of the number of children associated with this parent
+
+# User Story 10, Parent Child Index Link
+
+# As a visitor
+# When I visit a parent show page ('/parents/:id')
+# Then I see a link to take me to that parent's `child_table_name` page ('/parents/:id/child_table_name')
---
+++ spec/features/teams/index_spec.rb
@@ -0,0 +1,12 @@
+# User Story 3, Child Index 
+
+# As a visitor
+# When I visit '/child_table_name'
+# Then I see each Child in the system including the Child's attributes
+# (data from each column that is on the child table)
+
+# User Story 8, Child Index Link
+
+# As a visitor
+# When I visit any page on the site
+# Then I see a link at the top of the page that takes me to the Child Index
