b066304017b91736521f78fc05592df4001e5cb9
J Seymour <JustJakeSeymour@gmail.com>
2022-12-04 14:33:49 -0700

    write division index spec tests

--- spec/features/divisions/index_spec.rb
+++ spec/features/divisions/index_spec.rb
@@ -1,19 +1,45 @@
-# User Story 1, Parent Index 
+require 'rails_helper'
 
-# For each parent table
-# As a visitor
-# When I visit '/parents'
-# Then I see the name of each parent record in the system
+RSpec.describe "Division Index" do
+  before:each do
+    @a = Division.create!(name: "A Division", difficulty: 1, weekends: false)
+    @liquid_death = @a.teams.create!(name: "Liquid Death", won: 5, lost: 3, shootout_loss: 0, active_team: true)
+    @smartel = @a.teams.create!(name: "Smartel", won: 5, lost: 3, shootout_loss: 0, active_team: true)
+    @otters = @a.teams.create!(name: "Otters", won: 3, lost: 4, shootout_loss: 1, active_team: true)
+    @bison = @a.teams.create!(name: "Bison", won: 2, lost: 5, shootout_loss: 0, active_team: true)
+    @no_longer_a = @a.teams.create!(name: "No Longer", won: 0, lost: 7, shootout_loss: 0, active_team: false)
+  end
+  
+  describe "name of each division" do
+    it 'displays the name of each division' do
+      visit '/divisions'
 
-# User Story 6, Parent Index sorted by Most Recently Created 
+      expect(page).to have_content(@a.name)
+    end
+  end
 
-# As a visitor
-# When I visit the parent index,
-# I see that records are ordered by most recently created first
-# And next to each of the records I see when it was created
+  describe "names sorted by most recent first" do
+    it 'orders records by most recent first' do
+      @d2_mw = Division.create!(name: "D2 Mon/Wed", difficulty: 2, weekends: false)
 
-# User Story 9, Parent Index Link
+      visit '/divisions'
+      expect
+      # I see that records are ordered by most recently created first
+      expect
+      # And next to each of the records I see when it was created
+    end
+  end
 
-# As a visitor
-# When I visit any page on the site
-# Then I see a link at the top of the page that takes me to the Parent Index
+  describe "link to '/divisions'" do
+    it 'has a link to /divisions on multiple pages' do
+      # When I visit any page on the site
+      visit ""
+      # Then I see a link at the top of the page that takes me to the Parent Index
+      expect(page).to have_link("Divisions")
+      visit ""
+      expect(page).to have_link("Divisions")
+      visit ""
+      expect(page).to have_link("Divisions")
+    end
+  end
+end
